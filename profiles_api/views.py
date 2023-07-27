a62a6144988fd0218ec2e275fc9004b38b621d5b
Michael <michael.boitmann@gmail.com>
2023-07-27 21:07:53 +1200

    added viewset to profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -67,6 +67,33 @@ class HelloViewSet(viewsets.ViewSet):
     return Response({'message': 'Hello!', 'a_viewset': a_viewset})
 
 
-  def create(seld, request):
+  def create(self, request):
     """Create a new hellow message"""
-    serializer = self.setializer_class(data=request.data)
+    serializer = self.serializer_class(data=request.data)
+
+    if serializer.is_valid():
+      name = serializer.validated_data.get('name')
+      message = f"Hello {name}!"
+      return Response({ 'message': message})
+    else: 
+        return Response(
+          serializer.errors,
+          status=status.HTTP_400_BAD_REQUEST
+        )
+
+  def retrieve(self, request, pk=None):
+    """Handle getting an object by its ID"""
+    return Response({'http_method': 'GET'})
+
+  def update(self, request, pk=None):
+    """Handle updating an object"""
+    return Response({'http_method': 'PUT'})
+
+  def partial_update(self, request, pk=None):
+    """Handle updating part of an object"""
+    return Response({'http_method': 'PATCH'})
+
+  def destroy(self, request, pk=None):
+    """Handler removing an object"""
+    return Response({'http_method': 'DELETE'})
+
