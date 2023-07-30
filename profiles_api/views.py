faf242f1c4b5ee2a4d37f3f39ed56005d63c446f
Michael <michael.boitmann@gmail.com>
2023-07-30 22:43:51 +1200

    update on serializers and views files

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from profiles_api import serializers
+from profiles_api import models
 from rest_framework import viewsets
 
 
