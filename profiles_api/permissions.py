92987ab3a2145896faf5afedb192ed072a1fdf19
Michael <michael.boitmann@gmail.com>
2023-08-01 21:28:00 +1200

    aded user profile feed api:

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -21,3 +21,13 @@ class UpdateOwnStatus(permissions.BasePermission):
             return True
 
         return obj.user_profile.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to udpate their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the user is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile.id == request.user.id
